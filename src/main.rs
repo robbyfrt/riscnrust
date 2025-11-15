660461406b42171f625b30b77e5d4d71aff42388
robbyfrt <72253164+robbyfrt@users.noreply.github.com>
2025-11-15 22:44:48 +0100

    added wifi with mock AP ✅

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,76 @@
 use esp_idf_hal::gpio::PinDriver;
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_hal::delay::FreeRtos;
+use esp_idf_svc::{
+    log::EspLogger,
+    wifi::{
+        BlockingWifi,
+        EspWifi,
+        Configuration,
+        ClientConfiguration,
+        AuthMethod,
+    },
+    nvs::EspDefaultNvsPartition,
+    eventloop::EspSystemEventLoop,
+};
+use log::info;
 
+const SSID: &str = "Wokwi-GUEST";
+const PASSWORD: &str = "";
 
 fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise, some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
+    EspLogger::initialize_default();
 
     let peripherals = Peripherals::take()?;
     let mut led = PinDriver::output(peripherals.pins.gpio0)?;
     
+    let sys_loop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+
+    let mut wifi = BlockingWifi::wrap(
+        EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?,
+        sys_loop,
+    )?;
+
+    connect_wifi(&mut wifi)?;
+
+    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+    info!("Wifi DHCP info: {ip_info:?}");
+    info!("Shutting down in 5s...");
+
+    std::thread::sleep(core::time::Duration::from_secs(5));
+
     loop {
         led.set_high()?;
         FreeRtos::delay_ms(500);
         led.set_low()?;
         FreeRtos::delay_ms(500);
     }
+}
+
+fn connect_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> anyhow::Result<()> {
+    let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
+        ssid: SSID.try_into().unwrap(),
+        bssid: None,
+        auth_method: AuthMethod::None,
+        password: PASSWORD.try_into().unwrap(),
+        channel: None,
+        ..Default::default()
+    });
+
+    wifi.set_configuration(&wifi_configuration)?;
+
+    wifi.start()?;
+    info!("Wifi started");
+
+    wifi.connect()?;
+    info!("Wifi connected");
+
+    wifi.wait_netif_up()?;
+    info!("Wifi netif up");
+
+    Ok(())
 }
